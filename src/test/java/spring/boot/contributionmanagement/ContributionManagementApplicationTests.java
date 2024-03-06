c28d3e34a607390eb801e58f233a080d944dfba3
Phucngo-IT <ngohuynhphuc24.10@gmail.com>
2024-03-06 21:24:49 +0700

    Initial commit

---
+++ src/test/java/spring/boot/contributionmanagement/ContributionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.contributionmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContributionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
