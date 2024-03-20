cec1f6f5966b195349a7e8706bacc76bd53035af
KhiemHuynh123 <Khiemhhgcc210280@fpt.edu.vn>
2024-03-20 22:59:39 +0700

    pull from main

--- src/test/java/spring/boot/contributionmanagement/ContributionManagementApplicationTests.java
+++ src/test/java/spring/boot/contributionmanagement/ContributionManagementApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class git ContributionManagementApplicationTests {
+class ContributionManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
